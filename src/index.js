872a943be93bb8074ec69a82802d7271e6ee674b
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-04-02 21:37:01 +0530

    routes for google users

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 const express=require('express')
 const app = express()
 const connect = require('./config/db')
-
+const cors=require("cors")
 
 const port=process.env.PORT || 8000
 let {register,login} = require("./controller/authcontroller");
 
+const googlecontroller= require("./controller/google.user")
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
+app.use("/googleuser",googlecontroller)
+
 app.post("/register", register);
+
 app.post("/login",login);
 
 app.listen(port,async ()=>{
